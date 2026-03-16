9314368d410aebf886f79530210a56539de292b5
wx <wangxing031110@gmail.com>
2026-03-16 20:10:07 +0800

    Windows/kaifa修改main.cpp文件，添加打印333333333

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 int main()
 {
-	cout << "softdev" <<endl;
-	cout << 1111111111 <<endl;
-	cout << 222222222 <<endl;
+	cout << "softdev"  << endl;
+	cout << 1111111111 << endl;
+	cout << 2222222222 << endl;
+	cout << 3333333333 << endl;
 	return 0;
 }
